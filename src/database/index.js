91b07978daf566d6731671dab460bf66b99badea
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-08-27 23:42:25 -0300

    Configurando MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Sequelize from 'sequelize';
 
 import Appointment from '../app/models/Appointment';
@@ -11,12 +12,23 @@ class Database {
   constructor() {
     this.connection = new Sequelize(databaseConfig);
     this.init();
+    this.mongo();
   }
 
   init() {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
