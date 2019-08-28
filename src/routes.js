5355f7a403953dc939de3211e52ce88464e61e5e
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-08-27 21:13:07 -0300

    feat: user avatar

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.save);
 routes.post('/sessions', SessionController.save);
@@ -12,5 +16,6 @@ routes.post('/sessions', SessionController.save);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.save);
 
 export default routes;
