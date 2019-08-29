249bddb630e4ce5b3e6af6aab1d339a88d16929f
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-08-28 23:16:32 -0300

    Campos virtuais no agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -19,7 +19,7 @@ class AppointmentController {
         canceled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit,
       offset: (page - 1) * limit,
       include: [
