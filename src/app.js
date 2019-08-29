7aacf1a7ec6fa018ea6ec39453fd11ea80957e08
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-08-28 23:33:17 -0300

    Tratamento de exceçoes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
+import * as Sentry from '@sentry/node';
 import express from 'express';
 import { resolve } from 'path';
+import Youch from 'youch';
+
+import 'express-async-errors';
 
 import './database';
+import sentryConfig from './config/sentry';
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +31,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      res.status(500).json(errors);
+    });
   }
 }
 
