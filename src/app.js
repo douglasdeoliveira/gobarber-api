b69242f4ee96fee97ceac1de64913d4e4ad24dde
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-24 21:36:21 -0300

    utlizando Rate Limit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import * as Sentry from '@sentry/node';
 import cors from 'cors';
 import express from 'express';
+import RateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import { resolve } from 'path';
+import RateLimitRedis from 'rate-limit-redis';
+import redis from 'redis';
 import Youch from 'youch';
 
 import 'dotenv/config';
@@ -28,10 +31,26 @@ class App {
     this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
